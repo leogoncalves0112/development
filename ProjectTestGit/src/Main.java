9e804f61481c6d286e95dd81f7d3d7af5fc48549
Leonardo <Leonardo@leonardo-dell>
2013-07-15 22:28:46 -0300

    Segundo commit teste.

--- ProjectTestGit/src/Main.java
+++ ProjectTestGit/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Projeto no GIT v1.0");
+		System.out.println("Projeto no GIT v1.1");
+		System.out.println("Alterado no GitHub");
 	}
 
 }
